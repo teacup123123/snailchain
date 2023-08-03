19488b7e17befe823469c524177d72d20a5720de
DESKTOP-095MQDV\USER <cijy.mathai@gmail.com>
2023-08-03 22:05:47 +0300

    no message

--- current_eqn.py
+++ current_eqn.py
@@ -13,5 +13,5 @@ def residueI_eqnA5(phis_rad, phi_rad, alpha, Phi_ext_Phi0, epsilon_J):
     curr_alpha = alpha * np.sin(phis_rad)
     curr_unitaries = np.sin((phis_rad - 2 * np.pi * Phi_ext_Phi0) / 3)
     curr_snail = curr_unitaries + curr_alpha
-    curr_inductor = phi_rad - M * phis_rad
+    curr_inductor = Xi_J*(phi_rad - M * phis_rad)
     return curr_snail - curr_inductor  # should be zero
